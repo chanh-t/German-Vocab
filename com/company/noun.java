d147c50ea614ddd5307ae059ae4840935fd5afcf
chanh-t <81384798+chanh-t@users.noreply.github.com>
2023-04-19 15:05:10 -0700

    partially finished noun vocab test. verb vocab test finished

--- com/company/noun.java
+++ com/company/noun.java
@@ -1,18 +1,23 @@
 package com.company;
 
-final public class word {
-    String german;
-    String english;
-    String gender;
+final public class noun {
+    public String NOUN_GER;
+    public String NOUN_ENG;
+    public String NOUN_GENDER;
+    public verb NOUN_TO_VERB;
 
-    public word(String german, String english, String gender) {
-        this.german = german;
-        this.english = english;
-        this.gender = gender;
+    public noun(String german, String english, String gender) {
+        this.NOUN_GER = german;
+        this.NOUN_ENG = english;
+        this.NOUN_GENDER = gender;
+    }
+
+    public noun() {
+
     }
 
     @Override
     public String toString() {
-        return german;
+        return NOUN_GER;
     }
 }
